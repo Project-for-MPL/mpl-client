6d049a1d21931dc13419db4864b6e4bde342953f
senasoon <senasoon0@gmail.com>
2023-01-31 20:47:14 +0900

    Design: initial commit

---
+++ src/pages/PlaylistDetailpage.tsx
@@ -0,0 +1,18 @@
+import PlaylistDetailCover from "../components/playlist/detail/PlaylistDetailCover";
+import PlaylistDetailMusic from "../components/playlist/detail/PlaylistDetailMusic";
+
+const PlaylistDetailpage = () => {
+  return (
+    <>
+      <PlaylistDetailCover />
+      <PlaylistDetailMusic />
+      <PlaylistDetailMusic />
+      <PlaylistDetailMusic />
+      <PlaylistDetailMusic />
+      <PlaylistDetailMusic />
+      <PlaylistDetailMusic />
+    </>
+  );
+};
+
+export default PlaylistDetailpage;
